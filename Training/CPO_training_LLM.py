28c97b714a122774e7a4cb86a30417a76d2f4eda
AysanAghazadeh <aya34@pitt.edu>
2025-09-11 00:41:25 -0400

    changed

--- Training/CPO_training_LLM.py
+++ Training/CPO_training_LLM.py
@@ -62,7 +62,7 @@ def train(args):
     model, tokenizer = get_model(args)
     cpo_config = CPOConfig(beta=0.1,
                            output_dir=args.model_path+f'/my_{args.LLM}',)
-    train_dataset = get_train_LLAMA3_CPO_Dataloader(args)
+    train_dataset = get_train_LLM_CPO_Dataloader(args)
     tmp = train_dataset.train_test_split(test_size=0.1)
     train_dataset = tmp["train"]
     eval_dataset = tmp["test"]
