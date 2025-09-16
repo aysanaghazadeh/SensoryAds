06221754563d2f5f61199734e565c8c996e3b07f
AysanAghazadeh <aya34@pitt.edu>
2025-09-15 22:10:57 -0400

    changed

--- scripts/sensation_retrieval_MLLM.sh
+++ scripts/sensation_retrieval_MLLM.sh
@@ -1,1 +1,1 @@
-python inference.py --config_type=DEFAULT --MLLM_prompt=MLLM_Sensation_Retrieval.jinja --model_type=MLLM --MLLM=QWenVL --resume=True
+python inference.py --config_type=DEFAULT --MLLM_prompt=MLLM_Sensation_Retrieval.jinja --model_type=MLLM --MLLM=LLAVA16 --resume=True
