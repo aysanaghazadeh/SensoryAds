95ab8886afcffefa85479e56bc8041b3bf6d7aa2
AysanAghazadeh <aya34@pitt.edu>
2025-09-16 11:24:31 -0400

    changed

--- T2I_models/QWenImage.py
+++ T2I_models/QWenImage.py
@@ -4,19 +4,19 @@ from torch import nn
 from transformers import BitsAndBytesConfig
 
 
-class Flux(nn.Module):
+class QWenImage(nn.Module):
     def __init__(self, args):
-        super(Flux, self).__init__()
+        super(QWenImage, self).__init__()
         self.device = args.device
         quantization_config = BitsAndBytesConfig(
             load_in_8bit=True,
             bnb_8bit_compute_dtype=torch.float16
         )
-        self.pipeline = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev",
+        self.pipeline = DiffusionPipeline.from_pretrained("Qwen/Qwen-Image",
                                                            torch_dtype=torch.float16,
                                                           quantization_config=quantization_config)
-        self.pipeline = self.pipeline.to(device=args.device)
         self.args = args
+        self.pipeline = self.pipeline.to(device=args.device)
 
     def forward(self, prompt):
         image = self.pipeline(prompt,
