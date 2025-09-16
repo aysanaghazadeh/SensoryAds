0f6881bcab755e309387856f62b93bf1caa28b07
AysanAghazadeh <aya34@pitt.edu>
2025-09-16 12:22:01 -0400

    changed

--- T2I_models/QWenImage.py
+++ T2I_models/QWenImage.py
@@ -1,16 +1,17 @@
 from diffusers import FluxPipeline, DiffusionPipeline
 import torch
 from torch import nn
-from transformers import BitsAndBytesConfig
+from diffusers.quantizers import PipelineQuantizationConfig
 
 
 class QWenImage(nn.Module):
     def __init__(self, args):
         super(QWenImage, self).__init__()
         self.device = args.device
-        quantization_config = BitsAndBytesConfig(
-            load_in_8bit=True,
-            bnb_8bit_compute_dtype=torch.float16
+        quantization_config = PipelineQuantizationConfig(
+            quant_backend="bitsandbytes_8bit",
+            quant_kwargs={"load_in_8bit": True, "bnb_8bit_quant_type": "nf4", "bnb_8bit_compute_dtype": torch.bfloat16},
+            components_to_quantize=["transformer", "text_encoder_2"],
         )
         self.pipeline = DiffusionPipeline.from_pretrained("Qwen/Qwen-Image",
                                                            torch_dtype=torch.float16,
