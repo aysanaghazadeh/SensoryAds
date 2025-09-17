163adb5f02dc940228d9cbaaef9650ee241514f3
AysanAghazadeh <aya34@pitt.edu>
2025-09-17 14:51:28 -0400

    changed

--- LLMs/InternLM.py
+++ LLMs/InternLM.py
@@ -12,34 +12,31 @@ class InternLM(nn.Module):
         super(InternLM, self).__init__()
         self.args = args
         if not args.train:
-            # model_path = "internlm/internlm2_5-7b-chat"
-            # self.model = AutoModelForCausalLM.from_pretrained(model_path,
-            #                                                   torch_dtype=torch.float16,
-            #                                                   trust_remote_code=True,
-            #                                                   load_in_8bit=True)
-            # self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-            #
-            # self.model = self.model.eval()
-            self.tokenizer = AutoTokenizer.from_pretrained("internlm/internlm2-base-7b", trust_remote_code=True)
-            self.model = AutoModelForCausalLM.from_pretrained("internlm/internlm2-base-7b",
+            self.args = args
+            self.tokenizer = AutoTokenizer.from_pretrained("internlm/internlm3-8b-instruct", trust_remote_code=True)
+            self.model = AutoModelForCausalLM.from_pretrained("internlm/internlm3-8b-instruct",
                                                               torch_dtype=torch.float16,
                                                               load_in_8bit=True,
-                                                              trust_remote_code=True)
+                                                              trust_remote_code=True).eval()
             self.model = self.model.eval()
 
     def forward(self, prompt):
         if not self.args.train:
+            messages = [
+                {"role": "user", "content": prompt},
+            ]
+            tokenized_chat = self.tokenizer.apply_chat_template(messages,
+                                                                tokenize=True,
+                                                                add_generation_prompt=True,
+                                                                return_tensors="pt").to(self.args.device)
 
-            # length = 0
-            # for response, history in self.model.stream_chat(self.tokenizer, prompt, history=[]):
-            #     output = history[0][-1]
-            #     length = len(response)
-            # return output
-            inputs = self.tokenizer([prompt], return_tensors="pt")
-            for k, v in inputs.items():
-                inputs[k] = v.cuda()
-            gen_kwargs = {"temperature": 0.8, "do_sample": True}
-            output = self.model.generate(**inputs, **gen_kwargs)
-            output = self.tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
-            return output
+            generated_ids = self.model.generate(tokenized_chat)
 
+            generated_ids = [
+                output_ids[len(input_ids):] for input_ids, output_ids in zip(tokenized_chat, generated_ids)
+            ]
+            # prompt = self.tokenizer.batch_decode(tokenized_chat)[0]
+            print(f'User: {prompt}')
+            response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+            print(f'Assistant: {response}')
+            return response
