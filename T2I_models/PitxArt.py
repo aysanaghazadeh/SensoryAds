4505a8878a42f191759b22e56292a00a0c32e82e
AysanAghazadeh <aya34@pitt.edu>
2025-09-16 22:07:51 -0400

    changed

--- T2I_models/PitxArt.py
+++ T2I_models/PitxArt.py
@@ -1,12 +1,17 @@
 import torch
 from torch import nn
-from diffusers import PixArtAlphaPipeline
+from diffusers.quantizers import PipelineQuantizationConfig
 
 
 class PixArt(nn.Module):
     def __init__(self, args):
         super(PixArt, self).__init__()
         self.device = args.device
+        quantization_config = PipelineQuantizationConfig(
+            quant_backend="bitsandbytes_8bit",
+            quant_kwargs={"load_in_8bit": True, "bnb_8bit_quant_type": "nf4", "bnb_8bit_compute_dtype": torch.bfloat16},
+            components_to_quantize=["transformer", "text_encoder_2"],
+        )
         self.pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS",
                                                         torch_dtype=torch.float16)
         self.pipe = self.pipe.to(device=args.device)
