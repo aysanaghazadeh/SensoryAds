8e990706eb9e3767f573bf04154476c114b942d0
AysanAghazadeh <aya34@pitt.edu>
2025-09-17 16:24:31 -0400

    changed

--- T2I_models/QWenImage.py
+++ T2I_models/QWenImage.py
@@ -10,7 +10,7 @@ class QWenImage(nn.Module):
         self.device = args.device
         quantization_config = PipelineQuantizationConfig(
             quant_backend="bitsandbytes_4bit",
-            quant_kwargs={"load_in_4bit": True, "bnb_8bit_quant_type": "nf4", "bnb_4bit_compute_dtype": torch.bfloat16},
+            quant_kwargs={"load_in_4bit": True, "bnb_4bit_quant_type": "nf4", "bnb_4bit_compute_dtype": torch.bfloat16},
             components_to_quantize=["transformer", "text_encoder_2"],
         )
         self.pipe = DiffusionPipeline.from_pretrained("Qwen/Qwen-Image",
