b26dc2255038ddcb2ac783bebf3a9612d62f610f
AysanAghazadeh <aya34@pitt.edu>
2025-09-17 23:18:59 -0400

    changed

--- LLMs/LLAMA3_instruct.py
+++ LLMs/LLAMA3_instruct.py
@@ -28,14 +28,19 @@ class LLAMA3Instruct(nn.Module):
             else:
                 model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
-                self.pipeline = pipeline(
-                    "text-generation",
-                    model=model_id,
-                    token=os.environ.get('HF_TOKEN'),
-                    model_kwargs={"torch_dtype": torch.bfloat16},
-                    quantization_config=BitsAndBytesConfig(load_in_8bit=True),
-                    device_map="auto",
-                )
+                # self.pipeline = pipeline(
+                #     "text-generation",
+                #     model=model_id,
+                #     token=os.environ.get('HF_TOKEN'),
+                #     model_kwargs={"torch_dtype": torch.bfloat16},
+                #     quantization_config=BitsAndBytesConfig(load_in_8bit=True),
+                #     device_map="auto",
+                # )
+                self.model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct",
+                                                                  token=os.environ.get('HF_TOKEN'),
+                                                                  device_map='auto')
+                self.tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct",
+                                                               token=os.environ.get('HF_TOKEN'))
         else:
             self.model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct",
                                                               token=os.environ.get('HF_TOKEN'),
@@ -52,10 +57,30 @@ class LLAMA3Instruct(nn.Module):
                 {"role": "system", "content": "Be a helpful assistant"},
                 {"role": "user", "content": prompt},
             ]
-            output = self.pipeline(messages, max_new_tokens=250)
-            output = output[0]["generated_text"][-1]['content'].split('ASSISTANT:')[-1]
-            print('llama3 output:', output)
-            return output
+            input_ids = self.tokenizer.apply_chat_template(
+                messages,
+                add_generation_prompt=True,
+                return_tensors="pt"
+            ).to(self.model.device)
+
+            terminators = [
+                self.tokenizer.eos_token_id,
+                self.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+            ]
+
+            outputs = self.model.generate(
+                input_ids,
+                max_new_tokens=256,
+                eos_token_id=terminators,
+                do_sample=True,
+                temperature=0.6,
+                top_p=0.9,
+            )
+            response = outputs[0][input_ids.shape[-1]:]
+            response = self.tokenizer.decode(response, skip_special_tokens=True)
+            print(f'User: {prompt}')
+            print(f'Assistant: {response}')
+            return response
         else:
             messages = [
                 {"role": "system", "content": "Be a helpful assistant"},
