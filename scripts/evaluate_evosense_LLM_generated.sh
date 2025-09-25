29c72ceb432ed707d040399dc681cd0b0c1884ca
AysanAghazadeh <aya34@pitt.edu>
2025-09-24 20:55:08 -0400

    changed

--- scripts/evaluate_evosense_LLM_generated.sh
+++ scripts/evaluate_evosense_LLM_generated.sh
@@ -1,8 +1,8 @@
 python evaluation.py --config_type=DEFAULT \
 --evaluation_type=Evosense_LLM_generated \
---description_file=../experiments/results/SensoryAds/IN_InternVL_20250918_034952_Sensation_SD3_Sensation_description_generation.csv \
+--description_file=../experiments/results/SensoryAds/IN_InternVL_20250916_122348_AR_ALL_Flux_ALL_description_generation.csv \
 --LLM=LLAMA3_instruct \
 --fine_tuned=True \
 --model_checkpoint=21000 \
---T2I_model=SD3 \
+--T2I_model=FLUX \
 --resume=True
