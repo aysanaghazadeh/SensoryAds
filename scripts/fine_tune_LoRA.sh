ccd85e8650136c4f84af6e2d6ab6e9955e809707
AysanAghazadeh <aya34@pitt.edu>
2026-01-15 11:06:06 -0500

    changed

--- scripts/fine_tune_LoRA.sh
+++ scripts/fine_tune_LoRA.sh
@@ -6,13 +6,13 @@ accelerate launch --mixed_precision=bf16 train_Flux_LoRA.py \
   --caption_column="caption" \
   --output_dir="../models/flux_edit_lora" \
   --validation_prompt "Generate an image that evokes Comforting Warmth and conveys I should buy this car because it is luxurious." \
-  --validation_image "../Data/PittAd/train_images_total/0/13534.jpg" \
-  --validation_steps=250 \
+  --validation_image "../Data/PittAd/train_images_total/4/13534.jpg" \
+  --validation_steps=2100 \
   --resolution=1024 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
   --gradient_checkpointing \
   --learning_rate=1e-4 \
-  --rank=64 \
+  --rank=32 \
   --lr_scheduler="constant" \
   --max_train_steps=2000
