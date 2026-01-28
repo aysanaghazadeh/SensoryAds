cdd442e4441c9d8dcb04e423fd71514641222f97
AysanAghazadeh <aya34@pitt.edu>
2026-01-28 15:39:37 -0500

    changed

--- scripts/AR_image_generation_with_sensation.sh
+++ scripts/AR_image_generation_with_sensation.sh
@@ -1,10 +1,8 @@
 python inference.py --config_type=DEFAULT \
                     --inference_type=image_generation \
                     --text_input_type=AR \
-                    --T2I_model=SD3 \
+                    --T2I_model=AgenticEditing \
                     --T2I_prompt=AR_with_sensation.jinja \
                     --with_physical_sensation=True \
                     --test_set_sensation=train/sensation_annotations_parsed.json \
                     --test_set_QA=train/SensoryAd_Action_Reason.json \
-                    --fine_tuned=True \
-                    --model_checkpoint=500
