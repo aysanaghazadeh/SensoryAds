9653876a97b38b68fc0a69805cb705e3c35a24a9
AysanAghazadeh <aya34@pitt.edu>
2026-03-02 21:24:25 -0800

    changed

---
+++ scripts/evaluate_LLM_Judge.sh
@@ -0,0 +1,7 @@
+python evaluation.py --config_type=DEFAULT \
+--evaluation_type=LLM \
+--LLM=QWenLM \
+--model_type=LLM \
+--LLM_prompt=LLM_judge.jinja \
+--description_file=../experiments/results/SensoryAds/IN_QWenVL_train_images_total_ALL_description_generation.csv \
+--resume=True
