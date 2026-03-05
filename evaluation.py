6db9103de35ea3f76923113a1279bcf8bd7d0fa3
AysanAghazadeh <aya34@pitt.edu>
2026-03-04 23:47:08 -0800

    changed

--- evaluation.py
+++ evaluation.py
@@ -296,6 +296,43 @@ class SensationEvaluation:
                 scores[image_url][sensation] = score
             # print(f'{image_url} \n {json.dumps(scores[image_url], indent=4)}')
             json.dump(scores, open(result_file, 'w'))
+    
+    def evaluate_LLM_generated(self, args):
+        descriptions = pd.read_csv(args.description_file)
+        result_filename = f'gen_images_human_annotated_images_{args.LLM}.json'
+        directory_path = os.path.join(args.result_path, 'results', args.project_name, f'{args.evaluation_type}_{args.LLM}' )
+        os.makedirs(directory_path, exist_ok=True)
+        result_file = os.path.join(directory_path, result_filename)
+        if os.path.exists(result_file) and args.resume:
+            scores = json.load(open(result_file))
+            print(f'{result_file} already exists and {len(scores)} images are processed and will be skipped.')
+        else:
+            if os.path.exists(result_file):
+                scores = json.load(open(result_file))
+                print(f'{result_file} already exists, and {len(scores)} images will be overwritten.')
+            else:
+                print(f'{result_file} does not exist and will be created.')
+            scores = {}
+        for index, row in descriptions.iterrows():
+            image_url = row.ID
+            if image_url in scores:
+                print(f'{image_url} is already processed: {scores[image_url]}')
+                continue
+            description = row['description'].split('Q2:')[-1]
+            if image_url not in human_annotated_gen_images:
+                continue
+            model_name = args.T2I_model
+            image_url = '_'.join([model_name, image_url.split('/')[-1]])
+            for sensation in SENSATIONS_PARENT_MAP:
+                if image_url in scores and sensation in scores[image_url]:
+                    print(image_url)
+                    continue
+                if image_url not in scores:
+                    scores[image_url] = {}
+                score = get_LLM_Judge_Score(args, self.model, description, sensation)
+                scores[image_url][sensation] = score
+                print(f'{image_url} \n {json.dumps(scores[image_url], indent=4)}')
+                json.dump(scores, open(result_file, 'w'))
             
     def evaluate_MLLM_GT_Sensation(self, args):
         descriptions = pd.read_csv(args.description_file)
@@ -391,6 +428,7 @@ class SensationEvaluation:
                 scores[image_url][sensation] = score
                 print(f'{image_url} \n {json.dumps(scores[image_url], indent=4)}')
                 json.dump(scores, open(result_file, 'w'))
+                
 
     def evaluate(self, args):
         evaluation_name = 'evaluate_' + args.evaluation_type
