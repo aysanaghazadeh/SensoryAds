0cbb437d613b584c0f3c35e5a784da71853a1633
AysanAghazadeh <aya34@pitt.edu>
2026-09-13 15:49:20 -0700

    changed

--- scripts/HierarchicalCPOTraining.sh
+++ scripts/HierarchicalCPOTraining.sh
@@ -2,7 +2,6 @@ python train.py --config_type=DEFAULT \
 --training_type=HierarchicalCPO_train_LLM \
 --batch_size=1 \
 --LLM=LLAMA3_instruct \
---sensation_annotations=train/sensation_annotations_parsed.json \
---model_checkpoint=21000 \
---description_file=../experiments/results/SensoryAds/IN_InternVL_train_images_total_ALL_description_generation.csv
+--sensation_annotations=train/ExtendedRealAdAnnotationsClean_parsed.json \
+--description_file=../experiments/results/SensoryAds/IN_InternVL_PittAd_train_images_ALL_description_generation.csv
 
