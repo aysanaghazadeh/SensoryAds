17e80cad46d40fc8b7cb55bb558ba83d224ab7dd
AysanAghazadeh <aya34@pitt.edu>
2026-09-17 07:40:59 -0700

    chnaged

--- scripts/evaluate_Evosense_single_sensation.sh
+++ scripts/evaluate_Evosense_single_sensation.sh
@@ -3,7 +3,9 @@ python evaluation.py --config_type=DEFAULT \
 --description_file=../experiments/results/SensoryAds/IN_InternVL_20251123_225258_AR_ALL_SD3_ALL_description_generation.csv \
 --LLM=LLAMA3_instruct \
 --fine_tuned=True \
---model_checkpoint=21000 \
+--model_checkpoint=40000 \
+--evaluate_other_evoked_sensations=True \
+--test_set_sensation=train/sensation_annotations_parsed.json \
 --resume=True \
 --AD_type=ALL
 
@@ -11,13 +13,15 @@ python evaluation.py --config_type=DEFAULT \
 
 python evaluation.py --config_type=DEFAULT \
 --evaluation_type=Evosense_GT_Sensation \
---description_file=../experiments/results/SensoryAds/IN_InternVL_20250918_040133_Sensation_PixArt_Sensation_description_generation.csv \
+--description_file=../experiments/results/SensoryAds/IN_InternVL_20250917_185403_AR_ALL_QWenImage_ALL_description_generation.csv \
 --LLM=LLAMA3_instruct \
 --fine_tuned=True \
 --model_checkpoint=40000 \
 --model_name=my_HierarchicalCPO_data_annotation_LLAMA3_instruct \
---T2I_model=PixArt \
+--T2I_model=QwenImage \
 --AD_type=ALL \
+--evaluate_other_evoked_sensations=True \
+--test_set_sensation=train/sensation_annotations_parsed.json \
 --resume=True
 
 python evaluation.py --config_type=DEFAULT \
@@ -25,8 +29,22 @@ python evaluation.py --config_type=DEFAULT \
 --description_file=../experiments/results/SensoryAds/IN_InternVL_20260129_002256_AR_ALL_AgenticEditing_ALL_description_generation.csv \
 --LLM=LLAMA3_instruct \
 --fine_tuned=True \
---model_checkpoint=20000 \
+--model_checkpoint=40000 \
 --T2I_model=AgenticEditing \
+--evaluate_other_evoked_sensations=True \
+--test_set_sensation=train/sensation_annotations_parsed.json \
+--AD_type=ALL \
+--resume=True
+
+python evaluation.py --config_type=DEFAULT \
+--evaluation_type=Evosense_GT_Sensation \
+--description_file=../experiments/results/SensoryAds/IN_InternVL_20250916_122348_AR_ALL_Flux_ALL_description_generation.csv \
+--LLM=LLAMA3_instruct \
+--fine_tuned=True \
+--model_checkpoint=40000 \
+--T2I_model=AgenticEditing \
+--evaluate_other_evoked_sensations=True \
+--test_set_sensation=train/sensation_annotations_parsed.json \
 --AD_type=ALL \
 --resume=True
 
