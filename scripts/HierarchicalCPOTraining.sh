c693666c5820e473e149320794eb29d91149e987
AysanAghazadeh <aya34@pitt.edu>
2026-09-20 15:35:18 -0700

    fixed DDP bug

--- scripts/HierarchicalCPOTraining.sh
+++ scripts/HierarchicalCPOTraining.sh
@@ -15,7 +15,7 @@ fi
 # when the env is copied between machines.
 $PYTHON -m accelerate.commands.launch $LAUNCH_ARGS train.py --config_type=DEFAULT \
 --training_type=HierarchicalCPO_train_LLM \
---batch_size=1 \
+--batch_size=16 \
 --LLM=LLAMA3_instruct \
 --sensation_annotations=train/sensation_annotations_parsed_1200_train.json \
 --description_file=../experiments/results/SensoryAds/IN_InternVL_sensation_annotations_parsed_1200_train_ALL_real_description_generation.csv \
