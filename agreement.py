64a320c4f2c496492460aafe3b5316c60043175c
AysanAghazadeh <aya34@pitt.edu>
2026-09-20 00:46:12 -0700

    changed

--- agreement.py
+++ agreement.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     print('-' * 100)
     print('Evosense-LLAMA3-InternVL-11000')
     metrics = json.load(open(
-        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_extended_annotation_LLAMA3_instruct11000.json'))
+        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_extended_annotation_LLAMA3_instruct23000.json'))
     print(len(metrics))
     get_human_score_agreement(metrics, human_annotations)
     get_kappa_agreement(metrics, human_annotations)
