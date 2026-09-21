f85dbe2894a07ac3a91c05cd651e45b12c78c0db
AysanAghazadeh <aya34@pitt.edu>
2026-09-20 23:36:13 -0700

    fixed DDP bug

--- scripts/CPOTraining.sh
+++ scripts/CPOTraining.sh
@@ -2,7 +2,7 @@ python train.py --config_type=DEFAULT \
 --training_type=CPO_train_LLM \
 --batch_size=1 \
 --LLM=LLAMA3_instruct \
---sensation_annotations=train/sensation_annotations_parsed.json \
---description_file=../experiments/results/SensoryAds/IN_InternVL_train_images_total_ALL_description_generation.csv \
---model_checkpoint=18000
+--sensation_annotations=train/sensation_annotations_parsed_1200_train.json \
+--description_file=../experiments/results/SensoryAds/IN_InternVL_sensation_annotations_parsed_1200_train_ALL_real_description_generation.csv \
+--train_set_QA=train/sensation_annotations_parsed_1200_train.csv 
 
