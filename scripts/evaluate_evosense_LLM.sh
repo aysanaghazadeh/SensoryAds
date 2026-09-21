b4a81e2ee867f0d629b90a1d031e19444fe58fde
AysanAghazadeh <aya34@pitt.edu>
2026-09-20 22:31:12 -0700

    fixed DDP bug

--- scripts/evaluate_evosense_LLM.sh
+++ scripts/evaluate_evosense_LLM.sh
@@ -3,9 +3,8 @@ python evaluation.py --config_type=DEFAULT \
 --description_file=../experiments/results/SensoryAds/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation.csv \
 --LLM=LLAMA3_instruct \
 --fine_tuned=True \
---model_checkpoint=10500 \
+--model_checkpoint=3500 \
 --model_name=my_HierarchicalCPO_extended_annotation_LLAMA3_instruct \
 --resume=True \
 --sensation_annotations=train/sensation_annotations_parsed_1200_test.json
 
-/experiments/results/SensoryAds/Evosense_LLM/IN_InternVL_train_images_total_ALL_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_LLAMA3_instruct64500.json
