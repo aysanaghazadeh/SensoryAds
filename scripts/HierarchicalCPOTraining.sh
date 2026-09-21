c99ff1ffa212c6eb1aa89ed208413bf1b6f21d14
AysanAghazadeh <aya34@pitt.edu>
2026-09-20 23:38:23 -0700

    fixed DDP bug

--- scripts/HierarchicalCPOTraining.sh
+++ scripts/HierarchicalCPOTraining.sh
@@ -28,4 +28,5 @@ $PYTHON -m accelerate.commands.launch $LAUNCH_ARGS train.py --config_type=DEFAUL
 --sensation_annotations=train/sensation_annotations_parsed_1200_train.json \
 --description_file=$EXPERIMENTS_PATH/results/SensoryAds/IN_InternVL_sensation_annotations_parsed_1200_train_ALL_real_description_generation.csv \
 --train_set_QA=train/sensation_annotations_parsed_1200_train.csv \
---AD_type=ALL
+--AD_type=ALL \
+--model_checkpoint=4500
