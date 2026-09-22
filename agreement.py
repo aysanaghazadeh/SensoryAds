7705a9fdefd0fdba42ec2565e62c31397769f674
AysanAghazadeh <aya34@pitt.edu>
2026-09-21 21:52:01 -0700

    changed saving in EvoSenseLLM

--- agreement.py
+++ agreement.py
@@ -68,13 +68,14 @@ if __name__ == '__main__':
     # get_human_human_kappa_agreement(human_1_annotations, human_2_annotations)
     # get_human_human_score_agreement(human_1_annotations, human_2_annotations)
     print('-' * 100)
-    print('Evosense-LLAMA3-InternVL-176000')
+    print('Evosense-LLAMA3-InternVL-208000')
     metrics = json.load(open(
-        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_extended_annotation_LLAMA3_instruct11000.json'))
+        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_extended_annotation_LLAMA3_instruct13000.json'))
     print(len(metrics))
     get_human_score_agreement(metrics, human_annotations)
     get_kappa_agreement(metrics, human_annotations)
     get_krippendorff_agreement(metrics, human_annotations)
+    get_per_class_krippendorff_agreement(metrics, human_annotations)
     
     print('-' * 100)
     print('Evosense-LLAMA3-InternVL-160000')
