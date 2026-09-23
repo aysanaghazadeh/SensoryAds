1c519930e7a13f6aa6ce252c08ec012fac203e9f
AysanAghazadeh <aya34@pitt.edu>
2026-09-22 21:21:24 -0700

    changed saving in EvoSenseLLM

--- scripts/CPOTraining.sh
+++ scripts/CPOTraining.sh
@@ -1,6 +1,6 @@
 python train.py --config_type=DEFAULT \
 --training_type=CPO_train_LLM \
---batch_size=1 \
+--batch_size=8 \
 --LLM=LLAMA3_instruct \
 --sensation_annotations=train/sensation_annotations_parsed_1200_train.json \
 --description_file=../experiments/results/SensoryAds/IN_InternVL_sensation_annotations_parsed_1200_train_ALL_real_description_generation.csv \
