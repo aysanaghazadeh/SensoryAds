527bf5d00ebd5d918997bef670f0b01be3d7041f
AysanAghazadeh <aya34@pitt.edu>
2026-09-24 13:25:47 -0700

    changed

--- scripts/AIM.sh
+++ scripts/AIM.sh
@@ -1,7 +1,7 @@
 cd ../CAP
 python evaluate.py --config_type=DEFAULT \
 --evaluation_type=text_image_alignment \
---description_file=../experiments/results/SensoryAds/IN_InternVL_20260916_201638_AR_ALL_SD3_ALL_description_generation.csv \
+--description_file=../experiments/results/SensoryAds/IN_InternVL_20260922_111803_AR_ALL_QWenImage_ALL_description_generation.csv \
 --LLM=LLAMA3_instruct \
---result_file=SensoryAds/IN_InternVL_20260916_201638_AR_ALL_SD3_ALL_description_generation.csv \
+--result_file=SensoryAds/IN_InternVL_20260922_111803_AR_ALL_QWenImage_ALL_description_generation.csv \
 --fine_tuned=True
