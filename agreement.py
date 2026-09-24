3830898f3a1a408a20c54b81c6e6b00fdfe2510f
AysanAghazadeh <aya34@pitt.edu>
2026-09-24 08:28:52 -0700

    changed

--- agreement.py
+++ agreement.py
@@ -68,16 +68,6 @@ if __name__ == '__main__':
     # get_human_human_kappa_agreement(human_1_annotations, human_2_annotations)
     # get_human_human_score_agreement(human_1_annotations, human_2_annotations)
     print('-' * 100)
-    print('Evosense-LLAMA3-InternVL-336000')
-    metrics = json.load(open(
-        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_extended_annotation_LLAMA3_instruct21000.json'))
-    print(len(metrics))
-    get_human_score_agreement(metrics, human_annotations)
-    get_kappa_agreement(metrics, human_annotations)
-    get_krippendorff_agreement(metrics, human_annotations)
-    get_per_class_krippendorff_agreement(metrics, human_annotations)
-    
-    print('-' * 100)
     print('Evosense-LLAMA3-QWenVL-264000')
     metrics = json.load(open(
         '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_QWenVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_my_HierarchicalCPO_extended_annotation_LLAMA3_instruct16500.json'))
@@ -96,6 +86,47 @@ if __name__ == '__main__':
     get_kappa_agreement(metrics, human_annotations)
     get_krippendorff_agreement(metrics, human_annotations)
     get_per_class_krippendorff_agreement(metrics, human_annotations)
+    
+    print('-' * 100)
+    print('Evosense-QWenLM-InternVL-184000')
+    metrics = json.load(open(
+        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_QWenLM_finetunedTrue_my_HierarchicalCPO_extended_annotation_QWenLM11500.json'))
+    print(len(metrics))
+    get_human_score_agreement(metrics, human_annotations)
+    get_kappa_agreement(metrics, human_annotations)
+    get_krippendorff_agreement(metrics, human_annotations)
+    get_per_class_krippendorff_agreement(metrics, human_annotations)
+    
+    print('-' * 100)
+    print('Evosense-QWenLM-QWenVL-184000')
+    metrics = json.load(open(
+        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_QWenVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_QWenLM_finetunedTrue_my_HierarchicalCPO_extended_annotation_QWenLM11500.json'))
+    print(len(metrics))
+    get_human_score_agreement(metrics, human_annotations)
+    get_kappa_agreement(metrics, human_annotations)
+    get_krippendorff_agreement(metrics, human_annotations)
+    get_per_class_krippendorff_agreement(metrics, human_annotations)
+    
+    print('-' * 100)
+    print('Evosense-llama3-InternVL-SFT-Only-8000 * 16')
+    metrics = json.load(open(
+        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_mySFT_LLAMA3_instruct8000.json'))
+    print(len(metrics))
+    get_human_score_agreement(metrics, human_annotations)
+    get_kappa_agreement(metrics, human_annotations)
+    get_krippendorff_agreement(metrics, human_annotations)
+    get_per_class_krippendorff_agreement(metrics, human_annotations)
+    
+    print('-' * 100)
+    print('Evosense-llama3-InternVL-CPO-Only-7000 * 16')
+    metrics = json.load(open(
+        '/Users/aysanaghazadeh/experiments/results/SensoryAds/new_results/Evosense_LLM/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation_LLAMA3_instruct_finetunedTrue_myCPO_LLAMA3_instruct7000.json'))
+    print(len(metrics))
+    get_human_score_agreement(metrics, human_annotations)
+    get_kappa_agreement(metrics, human_annotations)
+    get_krippendorff_agreement(metrics, human_annotations)
+    get_per_class_krippendorff_agreement(metrics, human_annotations)
+    
     exit()
     print('-' * 100)
     print('Evosense-LLAMA3-InternVL-208000')
