60067b314ba2d017ae3dc69f7a2bdb7d41d426c3
AysanAghazadeh <aya34@pitt.edu>
2026-09-24 09:38:22 -0700

    changed

--- scripts/evaluate_evosense_LLM.sh
+++ scripts/evaluate_evosense_LLM.sh
@@ -3,9 +3,26 @@ python evaluation.py --config_type=DEFAULT \
 --description_file=../experiments/results/SensoryAds/IN_QWenVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation.csv \
 --LLM=QWenLM \
 --fine_tuned=True \
---model_checkpoint=12500 \
+--model_checkpoint=5000 \
 --model_name=my_HierarchicalCPO_extended_annotation_QWenLM \
 --sensation_annotations=train/sensation_annotations_parsed_1200_test.json
 
+python evaluation.py --config_type=DEFAULT \
+--evaluation_type=Evosense_LLM \
+--description_file=../experiments/results/SensoryAds/IN_QWenVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation.csv \
+--LLM=QWenLM \
+--fine_tuned=True \
+--model_checkpoint=6000 \
+--model_name=my_HierarchicalCPO_extended_annotation_QWenLM \
+--sensation_annotations=train/sensation_annotations_parsed_1200_test.json
+
+python evaluation.py --config_type=DEFAULT \
+--evaluation_type=Evosense_LLM \
+--description_file=../experiments/results/SensoryAds/IN_QWenVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation.csv \
+--LLM=QWenLM \
+--fine_tuned=True \
+--model_checkpoint=14000 \
+--model_name=my_HierarchicalCPO_extended_annotation_QWenLM \
+--sensation_annotations=train/sensation_annotations_parsed_1200_test.json
 
 
