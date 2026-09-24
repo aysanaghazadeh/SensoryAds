a906afdbf5d0dcb0b2a13017140c51bea540ac73
AysanAghazadeh <aya34@pitt.edu>
2026-09-24 08:29:28 -0700

    changed

--- scripts/evaluate_evosense_LLM.sh
+++ scripts/evaluate_evosense_LLM.sh
@@ -1,6 +1,6 @@
 python evaluation.py --config_type=DEFAULT \
 --evaluation_type=Evosense_LLM \
---description_file=../experiments/results/SensoryAds/IN_InternVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation.csv \
+--description_file=../experiments/results/SensoryAds/IN_QWenVL_sensation_annotations_parsed_1200_test_ALL_real_description_generation.csv \
 --LLM=QWenLM \
 --fine_tuned=True \
 --model_checkpoint=12500 \
